329435f5943dbc8d09b08f7fa9101cb3b5a2caf3
mihail58 <nafanai@96yandex.ru>
2022-12-18 18:46:40 +0300

    Третья задача первого семинара

---
+++ lesson1/example003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+if( number % 2 == 1 )
+  {
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.WriteLine(" является не четным");
+  }
+ else
+  {
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.WriteLine(" является четным");
+  }
