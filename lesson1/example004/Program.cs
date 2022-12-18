1b08b1bede95d8781cb52927dd3f6265dd4d3445
mihail58 <nafanai@96yandex.ru>
2022-12-18 19:08:55 +0300

    Четвертая задача первого семинара

---
+++ lesson1/example004/Program.cs
@@ -0,0 +1,25 @@
+﻿int i = 1;
+bool found = false;
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + number );
+while (i <= number)
+  {
+     if( i % 2 != 1 )
+       {
+         if( i == number )
+           {
+              Console.WriteLine( i );
+           }
+          else 
+           {
+              Console.Write( i + ", " );
+           } 
+         found = true;
+       }
+     i++;
+  }
+if( !found )
+  {
+     Console.WriteLine("Чётных чисел нет!");
+  } 
