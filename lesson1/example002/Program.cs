bdab7facac6770b4246dcda4429c9fed98192e29
mihail58 <nafanai@96yandex.ru>
2022-12-18 18:39:31 +0300

    Вторая задача первого семинара

---
+++ lesson1/example002/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+if((number2 < number1) && (number1 > number3))
+  {
+    Console.Write("Число ");
+    Console.Write(number1);
+    Console.WriteLine(" больше");
+  }
+ else if ((number1 < number2) && (number2 > number3))
+  {
+    Console.Write("Число ");
+    Console.Write(number2);
+    Console.WriteLine(" больше");}
+ else
+  {
+    Console.Write("Число ");
+    Console.Write(number3);
+    Console.WriteLine(" больше");
+  }
