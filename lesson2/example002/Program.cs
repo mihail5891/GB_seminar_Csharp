e6f9635e29d701c2d1aab14c23fee74fefdd0326
mihail58 <nafanai@96yandex.ru>
2022-12-19 18:53:03 +0300

    Решил вторую задачу второго семинара по C#

---
+++ lesson2/example002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if( number >= 100 )
+  {
+     while( number > 999 ) number = number / 10;
+     int result = number % 10;
+     Console.Write("Третья цифра вашего числа: ");
+     Console.WriteLine( result );
+  }
+ else
+  {
+    Console.WriteLine("Третьей цифры нет");
+  } 
