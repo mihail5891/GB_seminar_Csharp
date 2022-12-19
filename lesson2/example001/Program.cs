94c044b4abbac6b41a19eca96ed45252835e599d
mihail58 <nafanai@96yandex.ru>
2022-12-19 18:27:27 +0300

    Выполнил первую задачу второго семинара по C#

---
+++ lesson2/example001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if( number > 99 && number <= 999 )
+  {
+     int result = ((number / 10) % 10);
+     Console.Write("Вторая цифра вашего числа: ");
+     Console.WriteLine( result );
+  }
+ else
+  {
+      Console.WriteLine("Вы ввели не трехзначное число!");
+  }
