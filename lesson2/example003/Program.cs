1556f54e2b7a391ffa3e6a3d86a513d214fd1d70
mihail58 <nafanai@96yandex.ru>
2022-12-19 19:03:05 +0300

    Третья задача второго семинара по C#

---
+++ lesson2/example003/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число дня недели");
+int number = int.Parse(Console.ReadLine());
+if( number >=1 && number <= 5 )
+ {
+    Console.WriteLine("Этот день не является выходным");
+ }
+else if(number >=6 && number <= 7)
+ {
+    if( number == 6)
+      {
+         Console.WriteLine("Суббота - выходной");
+      }
+     else
+      {
+         Console.WriteLine("Воскресенье - выходной");
+      } 
+ } 
+else
+ {
+    Console.WriteLine("Такого дня недели не бывает!");
+ }  
