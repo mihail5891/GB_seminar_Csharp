02de5d65ddd113b3d621ec273720caf9ffb43a8f
mihail58 <nafanai@96yandex.ru>
2022-12-20 15:58:16 +0300

    Исправил код что бы последний элемент был без запятой

--- lesson4/example003/Program.cs
+++ lesson4/example003/Program.cs
@@ -1,19 +1,10 @@
-﻿// Функция ввода сообщения
-int InputInt( string message )
-  {
-     Console.Write(message);
-     string number = Console.ReadLine();
-     if( number == "" )
-       {
-         Console.WriteLine("Вы не ввели число!");
-         return InputInt( message );
-       }
-     return int.Parse( number );
-  }
-int len = InputInt("Введите длинну массива: ");
-int[] arr = new int[len];
-for( int i = 0; i < arr.Length; i++ )
+﻿
+int[] arr = new int[8];
+Console.Write( "[ " );
+int count = arr.Length -1;
+for( int i = 0; i <= count; i++ )
   {
     arr[i] = new Random().Next(1,9);
-    Console.WriteLine( arr[i] );
+    if( i == count ) Console.Write( arr[i] ); else Console.Write( arr[i] + ", " );
   }
+ Console.Write( " ]" ); 
