f0cca0662f1051212f13105eeafe78f555b3ed2c
mihail58 <nafanai@96yandex.ru>
2022-12-20 15:34:53 +0300

    Решил вторую задачу четвертого семинара по C#

---
+++ lesson4/example002/Program.cs
@@ -0,0 +1,38 @@
+﻿// Функция ввода сообщения
+int InputInt( string message )
+  {
+     Console.Write(message);
+     string number = Console.ReadLine();
+     if( number == "" )
+       {
+         Console.WriteLine("Вы не ввели число!");
+         return InputInt( message );
+       }
+     return int.Parse( number );
+  }
+// Функция подсчета цифр в числе
+int CountNum( int num )
+  {
+    int i = 0;
+    while( num > 0)
+      {
+        num /= 10;
+        i++;
+      }
+    return i;
+  }
+// Функция вывода суммы цифр в числе
+int Sum( int number, int count)
+  {
+     int sum = 0;
+     for (int i = 1; i <= count; i++)
+       {
+          sum += number % 10;
+          number /= 10;
+       }
+     return sum;
+  }
+int number = InputInt("Введите число: ");
+int count = CountNum(number);
+int result = Sum( number, count );
+Console.WriteLine( result );
