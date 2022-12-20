76d5013e4ede71e70263576e1a994d36a2558dda
mihail58 <nafanai@96yandex.ru>
2022-12-20 14:38:44 +0300

    Решил третью задачую ьретьего семинара по C#

---
+++ lesson3/example003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Функция ввода сообщения
+int InputInt( string message )
+  {
+     Console.Write(message);
+     string number = Console.ReadLine();
+     if( number == "" )
+       {
+         Console.WriteLine("Вы не ввели число!");
+         return InputInt( message );
+       }
+     return int.Parse( number );
+  }
+int number = InputInt("Введите число N: ");
+for (int i = 1; i <= number; i++) Console.WriteLine( i*i*i );
