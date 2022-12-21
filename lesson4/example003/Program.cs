430a3a017b3528b3cb3791cc44e07c99d6152ece
mihail58 <nafanai@96yandex.ru>
2022-12-21 20:09:35 +0300

    Добавил два метода в программу

--- lesson4/example003/Program.cs
+++ lesson4/example003/Program.cs
@@ -1,10 +1,20 @@
-﻿
-int[] arr = new int[8];
-Console.Write( "[ " );
-int count = arr.Length -1;
-for( int i = 0; i <= count; i++ )
+﻿// функция создания масива и заполнение его случайными числами
+int[] FillArray()
   {
-    arr[i] = new Random().Next(1,9);
-    if( i == count ) Console.Write( arr[i] ); else Console.Write( arr[i] + ", " );
+     int[] arr = new int[8];
+     for( int i = 0; i < arr.Length; i++ ) arr[i] = new Random().Next(1,9);
+     return arr;      
   }
- Console.Write( " ]" ); 
+// функция вывода масива в консоль  
+void PrintArray(int[] arr )
+  {
+    Console.Write( "[ " );
+    int count = arr.Length -1;
+    for( int i = 0; i <= count; i++ )
+      {
+         if( i == count ) Console.Write( arr[i] ); else Console.Write( arr[i] + ", " );
+      }
+    Console.Write( " ]" ); 
+  }  
+int[] array = FillArray();
+PrintArray(array); 
