b4965c4765070c870f042a317feacf90475230a9
mihail58 <nafanai@96yandex.ru>
2022-12-21 20:01:06 +0300

    Добавил метод подсчета и вывода результата в консоль

--- lesson4/example001/Program.cs
+++ lesson4/example001/Program.cs
@@ -11,8 +11,14 @@ int InputInt( string message )
        }
      return int.Parse( number );
   }
+// Функция подсчета и вывода результата  
+void OutResult(int a, int b)
+  {
+    int result = 1;
+    for (int i = 1; i <= b; i++) result = result * a;
+    Console.WriteLine(result);
+  }
 int numberA = InputInt("Введите число A: ");
 int numberB = InputInt( "Введите число B: ");
-int result = 1;
-for (int i = 1; i <= numberB; i++) result = result * numberA;
-Console.WriteLine(result);
+
+OutResult(numberA,numberB);
