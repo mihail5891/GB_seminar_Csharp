c29dcb2ad3f787f2a707d1e4b1755cc73079e8d1
mihail58 <nafanai@96yandex.ru>
2022-12-22 19:21:07 +0300

    Решил первую задачу шестого семинара по C#

---
+++ lesson6/example001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Функция ввода сообщения
+string[] Input( string message )
+  {
+     Console.Write(message);
+     string[] text = Console.ReadLine().Split();
+     int count = text.Length;
+     if( count == 0 )
+       {
+         Console.WriteLine("Вы не ввели число!");
+         return Input( message );
+       }
+     return text;
+  }
+//функция подсчета чисел больше 0  
+int CountNumber( string[] arr )
+ {
+    int count = 0;
+    int[] num = Array.ConvertAll( arr, s => int.TryParse(s, out var x) ? x : -1 );
+    int Length = num.Length;
+    for (int i = 0; i < Length; i++)
+      {
+         if(  num[i] > 0) count++;
+      }
+      Console.WriteLine();
+    return count;  
+ }
+string[] arr = Input("Введите числа (через пробел): "); 
+int count = CountNumber( arr );
+Console.WriteLine($"Кол-во элементов > 0: {count}");
