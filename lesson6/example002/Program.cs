cfc502cb994b73957772754506e310bc7325f7a5
mihail58 <nafanai@96yandex.ru>
2022-12-22 19:48:11 +0300

    Решил вторую задачу шестого семинара по C#

---
+++ lesson6/example002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Функция ввода сообщения
+double InputInt( string message )
+  {
+     Console.Write(message);
+     string number = Console.ReadLine();
+     if( number == "" )
+       {
+         Console.WriteLine("Вы не ввели число!");
+         return InputInt( message );
+       }
+     return Convert.ToDouble( number );
+  }
+ // Функция подсчета и вывода результата в консоль 
+void OutResult( double b1, double b2, double k1, double k2)
+  {
+     double x = -(b1 - b2) / (k1 - k2);//double
+     double y = k1 * x + b1;
+     x = Math.Round(x, 3);
+     y = Math.Round(y, 3);
+     Console.WriteLine($"Пересечение в точке: ( {x}; {y})");
+  }
+double b1 = InputInt("Введите b1: ");
+double b2 = InputInt("Введите b2: ");
+double k1 = InputInt("Введите k1: ");
+double k2 = InputInt("Введите k2: ");
+OutResult(b1,b2,k1,k2);
+
+
