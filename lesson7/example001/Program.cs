24161a7139a05790ad57eff743c867d3c8cef587
mihail58 <nafanai@96yandex.ru>
2022-12-24 13:36:44 +0300

    Решил первую задачу седьмого семинара по C#

---
+++ lesson7/example001/Program.cs
@@ -0,0 +1,39 @@
+﻿// Функция ввода сообщения
+int InputInt( string message )
+  {
+     Console.Write(message);
+     string number = Console.ReadLine();
+     if( number == "" )
+       {
+         Console.WriteLine("Вы не ввели число!");
+         return InputInt( message );
+       }
+     return int.Parse( number );
+  }
+// функция наполнения массива рандомными числами  
+void FillArray(double[,] arr )
+  { 
+    Random rnd = new Random();
+    for( int i = 0; i < arr.GetLength(0); i++)
+      { 
+        for( int j = 0; j < arr.GetLength(1); j++)
+          { 
+             arr[i,j] = new Random().Next(-10,100);
+          }
+      }
+  }
+// функция вывода двумерного массива  
+void PrintArray(double[,] arr)
+  { 
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+      { 
+         for (int j = 0; j < arr.GetLength(1); j++) Console.Write($"{arr[i, j]} ");
+         Console.WriteLine();
+      }
+  }
+int m = InputInt("Задайте количество строк двумерного массива: ");
+int n = InputInt("Задайте количество столбцов двумерного массива: ");
+double[,] array = new double[m, n];
+FillArray( array );
+PrintArray(array);
