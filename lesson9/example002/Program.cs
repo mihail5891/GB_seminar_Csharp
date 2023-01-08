c1f55e58bdd5c4a31c14651b7b686b984a4509fd
mihail58 <nafanai@96yandex.ru>
2023-01-08 19:11:48 +0300

    Решил вторую задачу девятого семинара по C#

---
+++ lesson9/example002/Program.cs
@@ -0,0 +1,26 @@
+﻿// Функция ввода сообщения
+int InputInt( string message )
+  {
+     Console.Write( message );
+     string number = Console.ReadLine();
+     if( number == "" )
+       {
+         Console.WriteLine("Вы не ввели число!");
+         return InputInt( message );
+       }
+     return int.Parse( number );
+  }
+int CountSum( int m, int n )
+  {
+     if( m == n ) return n;
+     return n + CountSum( m, n - 1 );
+  }
+int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+int result = CountSum( m, n );
+Console.WriteLine($"Сумма элементов от {m} до {n} = {result}");
+
+
+
+
+
